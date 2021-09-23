96db9e67dd7a807816aa5e617881a6dfb8a62193
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-09-23 22:20:55 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -8,7 +8,7 @@ import chromedriver_binary
 #driver = webdriver.Chrome()#(options=options)
 
 chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 #chrome_options.add_argument('--disable-gpu')
